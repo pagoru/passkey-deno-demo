d76a329af9fd45680a0b1382c88054ee656b41b9
Pablo González <pagoru@gmail.com>
2023-07-06 20:21:11 +0200

    register working

--- projects/api/src/main.ts
+++ projects/api/src/main.ts
@@ -1,90 +1,114 @@
 import { Application, Router } from "oak/mod.ts";
 import { oakCors } from "cors";
-import { cryptoRandomString  } from "crypto_random_string";
+import { cryptoRandomString } from "crypto_random_string";
 import { Fido2Lib } from "fido2";
 
 const kv = await Deno.openKv();
 
-// kv.delete(["users", "alice"])
-// const res = await kv.get(["users", "alice"]);
-
 const router = new Router();
 
 const f2l = new Fido2Lib({
-	timeout: 60000,
-	rpId: "web.local",
-	rpName: "Testing auth",
-	// rpIcon: "https://example.com/logo.png",
-	challengeSize: 128,
-	attestation: "none",
-	cryptoParams: [-7, -257],
-	authenticatorAttachment: "platform",
-	authenticatorRequireResidentKey: false,
-	authenticatorUserVerification: "required"
+  timeout: 60000,
+  rpId: "web.local",
+  rpName: "Testing auth",
+  // rpIcon: "https://example.com/logo.png",
+  challengeSize: 128,
+  attestation: "none",
+  cryptoParams: [-7, -257],
+  authenticatorAttachment: "platform",
+  authenticatorRequireResidentKey: false,
+  authenticatorUserVerification: "required",
 });
 
 router
-	.get("/register", async (context) => {
-		const challenge = cryptoRandomString({ length: 64 });
-		const userId = cryptoRandomString({ length: 32 });
-		
-		const username = context.request.url.searchParams.get('username');
-		
-		const res = await kv.get(["users", username]);
-		if(res && res.status === 'verified') {
-			context.response.body = {
-				error: true,
-				error_description: 'User already registered!'
-			}
-			return;
-		}
-		console.log('challenge v1', challenge)
-		await kv.set(["users", username], { challenge, userId, status: 'pending' });
-		
-		const registrationOptions = await f2l.attestationOptions();
-		registrationOptions.challenge = challenge
-		registrationOptions.user = {
-			id: userId,
-			name: 'name-' + userId, // if use email...
-			displayName: 'displayName-' + userId,
-		}
-		
-		context.response.body = registrationOptions;
-	})
-	.post("/register", async (context) => {
-		const credential = await context.request.body().value;
-		credential.rawId = new Int32Array(credential.rawId).buffer;
-		// credential.response.clientDataJSON = new Int32Array(credential.response.clientDataJSON).buffer;
-		// credential.response.attestationObject = new Int32Array(credential.response.attestationObject).buffer;
-		// credential.rawId = b_ab( b64url_b(credential.rawId) )
-		// console.log(credential)
-		
-		console.log(credential)
-		
-		credential.response.clientDataJSON = JSON.parse(atob(credential.response.clientDataJSON));
-		credential.response.clientDataJSON.challenge = atob(credential.response.clientDataJSON.challenge);
-		credential.response.clientDataJSON = btoa(JSON.stringify(credential.response.clientDataJSON));
-		
-		// console.log(atob(JSON.parse(atob(credential.response.clientDataJSON)).challenge));
-		
-		let user;
-		
-		const iter = await kv.list<string>({ prefix: ["users"] });
-		for await (const currentUser of iter) {
-			if(currentUser.value.userId === credential.userId) user = currentUser
-		}
-		
-		// console.log(user.value.challenge)
-		
-		// console.log(user);
-		const attestationExpectations = {
-			challenge: user.value.challenge,
-			origin: "https://web.local",
-			factor: "either"
-		};
-		const regResult = await f2l.attestationResult(credential, attestationExpectations);
-		console.log(regResult)
-	})
+  .get("/register", async (context) => {
+    const challenge = cryptoRandomString({ length: 64 });
+    const userId = cryptoRandomString({ length: 32 });
+
+    const username = context.request.url.searchParams.get("username");
+
+    const res = await kv.get(["users", username]);
+    console.log(res && res.value.status);
+    if (res && res.value.status === "verified") {
+      context.response.status = 409;
+      return;
+    }
+    console.log("challenge v1", challenge);
+    await kv.set(["users", username], { challenge, userId, status: "pending" });
+
+    const registrationOptions = await f2l.attestationOptions();
+    registrationOptions.challenge = challenge;
+    registrationOptions.user = {
+      id: userId,
+      name: "name-" + userId, // if use email...
+      displayName: "displayName-" + userId,
+    };
+
+    context.response.body = registrationOptions;
+  })
+  .post("/register", async (context) => {
+    try {
+      const { credential, userId } = await context.request.body().value;
+
+      const base64ToBuffer = (base64String: string): Uint8Array => {
+        const decodedString = atob(base64String);
+        const uint8Array = new Uint8Array(decodedString.length);
+
+        for (let i = 0; i < decodedString.length; i++) {
+          uint8Array[i] = decodedString.charCodeAt(i);
+        }
+
+        return uint8Array.buffer;
+      };
+
+      const attestation = {
+        id: credential.id,
+        rawId: base64ToBuffer(credential.rawId),
+        response: {
+          clientDataJSON: credential.response.clientDataJSON,
+          attestationObject: base64ToBuffer(
+            credential.response.attestationObject,
+          ),
+        },
+        type: credential.type,
+      };
+
+      let user;
+
+      const iter = await kv.list<string>({ prefix: ["users"] });
+      for await (const currentUser of iter) {
+        if (currentUser.value.userId === userId) {
+          user = currentUser;
+        }
+      }
+
+      const attestationExpectations = {
+        challenge: btoa(user.value.challenge),
+        origin: "https://web.local",
+        factor: "either",
+      };
+
+      const regResult = await f2l.attestationResult(
+        attestation,
+        attestationExpectations,
+      );
+      const authnrData = regResult.authnrData;
+
+      const credId = authnrData.get("credId"); // ArrayBuffer
+      const counter = authnrData.get("counter"); // int
+      const publicKey = authnrData.get("credentialPublicKeyPem"); // string
+
+      await kv.set(user.key, {
+        challenge: undefined,
+        authnr: { credId, counter, publicKey },
+        status: "verified",
+      });
+      context.response.status = 200;
+    } catch (e) {
+      console.error(e);
+      context.response.status = 500;
+    }
+  });
 
 const app = new Application();
 
--- projects/web/src/modules/app.tsx
+++ projects/web/src/modules/app.tsx
@@ -3,41 +3,66 @@ import React, { useState } from "react";
 export const App = () => {
   const [username, setUsername] = useState<string>("pagoru");
 
-  const ab_b64 = (buf) =>
-    btoa(buf.reduce((data, val) => data + String.fromCharCode(val), ""));
-
   const createAccount = async () => {
-    let response = await fetch(`https://api.local/register?username=${username}`)
+    let response = await fetch(
+      `https://api.local/register?username=${username}`,
+    );
     const credentialsCreateProps = await response.json();
-    credentialsCreateProps.challenge = new TextEncoder().encode(credentialsCreateProps.challenge);
-    credentialsCreateProps.user.id = new TextEncoder().encode(credentialsCreateProps.user.id);
-    
+    credentialsCreateProps.challenge = new TextEncoder().encode(
+      credentialsCreateProps.challenge,
+    );
+    credentialsCreateProps.user.id = new TextEncoder().encode(
+      credentialsCreateProps.user.id,
+    );
+
     const credential = (await navigator.credentials.create({
       publicKey: credentialsCreateProps,
     })) as any;
+
+    console.log(credential);
 
     // console.log("credential", credential);
     // console.log("credential.rawId", credential.rawId);
     // console.log("credential2", ab_b64(new Uint8Array(credential.rawId)));
 
-    console.log(credential.response.clientDataJSON)
+    const clientDataJSON = JSON.parse(
+      new TextDecoder().decode(
+        new Uint8Array(credential.response.clientDataJSON),
+      ),
+    );
+
+    const bufferToBase64 = (arrayBuffer: Uint8Array): string =>
+      btoa(String.fromCharCode.apply(null, new Uint8Array(arrayBuffer)));
+
     const passableCredential = {
       id: credential.id,
-      rawId: new Uint8Array(credential.rawId),
+      rawId: bufferToBase64(new Uint8Array(credential.rawId)),
       response: {
-        clientDataJSON: ab_b64(new Uint8Array(credential.response.clientDataJSON)),
-        attestationObject: ab_b64(new Uint8Array(credential.response.attestationObject)),
+        clientDataJSON: btoa(
+          JSON.stringify({
+            challenge: atob(clientDataJSON.challenge),
+            ...clientDataJSON,
+          }),
+        ),
+        attestationObject: bufferToBase64(
+          new Uint8Array(credential.response.attestationObject),
+        ),
       },
-      userId: new TextDecoder().decode(credentialsCreateProps.user.id),
       type: credential.type,
     };
-    response = await fetch('https://api.local/register', {
-      method: 'POST',
+    console.log(atob(clientDataJSON.challenge), passableCredential);
+    response = await fetch("https://api.local/register", {
+      method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(passableCredential)
-    })
+      body: JSON.stringify({
+        credential: passableCredential,
+        userId: new TextDecoder().decode(
+          new Uint8Array(credentialsCreateProps.user.id),
+        ),
+      }),
+    });
   };
 
   const onRegister = async (event) => {
